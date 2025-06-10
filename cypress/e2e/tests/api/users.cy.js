6c7a8ad6adabbd0f052b4f92e8acc85dcb13f5ca
IvsonC <ivson.costa@thoughtworks.com>
2025-06-10 11:23:52 -0300

    Adding the comments inside the user.cy.js in order to explain the tests and removing a console log without usage

--- cypress/e2e/tests/api/users.cy.js
+++ cypress/e2e/tests/api/users.cy.js
@@ -1,3 +1,9 @@
+/**
+ * Users API Test Suite
+ * This test suite verifies the user management functionality of the API
+ * It specifically checks if the API can create new users and handle user data correctly
+ */
+
 const options = { env: { snapshotOnly: true } };
 describe("List Users", options, () => {
   it("Should list users", () => {
@@ -11,12 +17,20 @@ describe("List Users", options, () => {
 });
 
 describe("Add User", options, () => {
-  it ("Should add a new user", () => {
+  /**
+   * Test: Create New User
+   * Steps:
+   * 1. Generate random test data for a new user (email, password, name, etc.)
+   * 2. Make a POST request to create a new user with the generated data
+   * 3. Verify the response status is 201 (Created)
+   * 4. Verify the success message in the response
+   * 5. Verify the response contains a user ID
+   */
+  it("Should add a new user", () => {
     cy.generateTestData().then((testData) => {
       cy.apiAddUser(testData).then((response) => {
         expect(response.status).to.eq(201);
         expect(response.body.message).to.eq('Cadastro realizado com sucesso');
-        console.log(response.body);
       });
     });
   });
